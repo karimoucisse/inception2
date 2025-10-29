274a341e32bcfd2896abdec52e28ada6327346c0
karimou cisse <karimou.cisse1@gmail.com>
2025-10-29 22:14:14 +0100

    add big changes

--- srcs/rm-bash.sh
+++ srcs/rm-bash.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo docker compose down
+docker compose down
 
-sudo docker rmi -f $(sudo docker image ls -qa)
+docker rmi -f $(docker image ls -qa)
 
-sudo docker volume rm -f $(sudo docker volume ls -q)
+docker volume rm -f $(docker volume ls -q)
 
 # sudo docker exec -it wordpress bash
 
